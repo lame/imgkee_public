6a2373d3b81f24419f9fac90900dff7f04563bbd
Daniel Timpone <dannytimpone@Gmail.com>
2014-03-27 19:02:05 -0400

    more pages added. some style fixes

--- app/static/js/imgkee.js
+++ app/static/js/imgkee.js
@@ -1,11 +1,5 @@
-// $(document).ready(function(){
-//   $(document).mousemove(function(e){
-//      TweenLite.to($('body'), 
-//         .5, 
-//         { css: 
-//             {
-//                 // 'background-position':parseInt(event.pageX/8) + "px "+parseInt(event.pageY/12)+"px, "+parseInt(event.pageX/15)+"px "+parseInt(event.pageY/15)+"px, "+parseInt(event.pageX/30)+"px "+parseInt(event.pageY/30)+"px"
-//             }
-//         });
-//   });
-// });
+ $(document).ready(function(){
+ 	click : function(){
+ 		
+ 	}
+ });

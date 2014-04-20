2336ae26d4df7164224646df9b7425073ed7585f
danieltimpone <dannytimpone@gmail.com>
2014-04-20 18:28:43 -0400

    nothing happening here

--- app/views.py
+++ app/views.py
@@ -5,9 +5,38 @@ from forms import LoginForm, RegistrationForm
 from models import User, ROLE_USER, ROLE_ADMIN
 import binascii, hashlib, urllib, cStringIO
 
+UPLOAD_FOLDER = 'butts'
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        file = request.files['file']
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_file',
+                                    filename=filename))
+    return '''
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form action="" method=post enctype=multipart/form-data>
+      <p><input type=file name=file>
+         <input type=submit value=Upload>
+    </form>
+    '''
+
 @app.route('/create_acct/' , methods=['GET','POST'])
 def create_acct():
 	form = RegistrationForm(request.form)
+	upload_file()
 	if form.validate_on_submit():
 		print form
 		user = User()
@@ -72,6 +101,7 @@ def internal_error(error):
 def internal_error(error):
     db.session.rollback()
     return render_template('500.html'), 500
+
 
 def dump():
 	ff = open("cat.png", "rb")
